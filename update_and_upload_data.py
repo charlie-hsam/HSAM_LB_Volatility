618ddf0e1cebce8c8c890813c83b5a777c5558ce
Charlie de La Pasture <charlie@hsam.co.za>
2025-03-06 18:05:21 +0200

    Save work before push

--- update_and_upload_data.py
+++ update_and_upload_data.py
@@ -1,3 +1,4 @@
+import os
 from extract_table_data import cycle_and_store_table_data
 from extract_chart_data import cycle_and_store_chart_data
 
@@ -8,4 +9,8 @@ WEIGHT_STEP = 10
 if __name__ == '__main__':
     cycle_and_store_table_data()
     cycle_and_store_chart_data()
-
+    os.system("git add "
+              "chart_and_table_data/volatility_chart_data.json "
+              "chart_and_table_data/volatility_table_data.json")
+    os.system('git commit -m "Updated JSON data from python script"')
+    os.system("git push origin main")
